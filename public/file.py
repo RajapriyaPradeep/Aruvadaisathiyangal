27921811e663ce3e56aa89a43aa9844de6068d03
Raja Priya <rajapriyamariappan@gmail.com>
2024-12-24 13:17:39 -0700

    some audio files

--- public/file.py
+++ public/file.py
@@ -17,7 +17,7 @@ import os
 #     return file_names
 
 # # Example usage
-# folder_path = "C:/Users/rajap/Downloads/SIN-20241224T185733Z-001"
+# folder_path = "C:/Users/rajap/Downloads/CHRISTIAN CONCEPTS-20241224T200024Z-001/CHRISTIAN CONCEPTS"
 # file_names = extract_file_names(folder_path)
 
 # print("Files found:")
@@ -79,26 +79,26 @@ import os
 #--------------------------------------------------------
 #Extract the file name uploaded in the github
 #--------------------------------------------------------
-def extract_file_names(directory):
-    """
-    Extracts file names from all files in a directory, including subfolders.
+# def extract_file_names(directory):
+#     """
+#     Extracts file names from all files in a directory, including subfolders.
     
-    Parameters:
-        directory (str): Path to the directory to scan.
+#     Parameters:
+#         directory (str): Path to the directory to scan.
         
-    Returns:
-        list: List of file names (with extensions) in the directory.
-    """
-    file_names = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_names.append(file)
-    return file_names
+#     Returns:
+#         list: List of file names (with extensions) in the directory.
+#     """
+#     file_names = []
+#     for root, _, files in os.walk(directory):
+#         for file in files:
+#             file_names.append(file)
+#     return file_names
 
-# Example usage
-folder_path = "C:/Users/rajap/Downloads/SIN-20241224T185733Z-001/SIN_for_website"
-file_names = extract_file_names(folder_path)
+# # Example usage
+# folder_path = "C:/Users/rajap/Downloads/UNDERSTANDING GREAT TRIBULATION-20241224T183431Z-001/websiteUGT"
+# file_names = extract_file_names(folder_path)
 
-print("Files found:")
-for name in file_names:
-    print(name)
+# print("Files found:")
+# for name in file_names:
+#     print(name)
