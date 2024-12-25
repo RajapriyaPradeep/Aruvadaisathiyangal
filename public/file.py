87606a9d04beedde8f6490c91b52c26b1008de3f
Raja Priya <rajapriyamariappan@gmail.com>
2024-12-25 15:03:10 -0700

    some audio files

--- public/file.py
+++ public/file.py
@@ -17,7 +17,7 @@ import os
 #     return file_names
 
 # # Example usage
-# folder_path = "C:/Users/rajap/Downloads/END TIME PROPHECY-20241225T001307Z-001/END TIME PROPHECY"
+# folder_path = "C:/Users/rajap/Downloads/PROPHETIC-20241225T001714Z-001/PROPHETIC/EZEKIAL PROPHECY/STUDY ON OT PROPHETS & ON PROPHECY BOOKS/AMR"
 # file_names = extract_file_names(folder_path)
 
 # print("Files found:")
@@ -70,8 +70,8 @@ import os
 #             print(f"Copied and renamed: {old_file_path} -> {new_file_path}")
 
 # # Example usage
-# source_folder = "C:/Users/rajap/Downloads/END TIME PROPHECY-20241225T001307Z-001/convert"
-# destination_folder = "C:/Users/rajap/Downloads/END TIME PROPHECY-20241225T001307Z-001/rename"
+# source_folder = "C:/Users/rajap/Downloads/PROPHETIC-20241225T001714Z-001/PROPHETIC/EZEKIAL PROPHECY/convert1"
+# destination_folder = "C:/Users/rajap/Downloads/PROPHETIC-20241225T001714Z-001/PROPHETIC/EZEKIAL PROPHECY/rename1"
 # copy_and_rename_files(source_folder, destination_folder)
 
 #--------------------------------------------------------
@@ -94,7 +94,7 @@ import os
 #     return file_names
 
 # # Example usage
-# folder_path = "C:/Users/rajap/Downloads/END TIME PROPHECY-20241225T001307Z-001/rename"
+# folder_path = "C:/Users/rajap/Downloads/PROPHETIC-20241225T001714Z-001/PROPHETIC/EZEKIAL PROPHECY/rename1"
 # file_names = extract_file_names(folder_path)
 
 # print("Files found:")
